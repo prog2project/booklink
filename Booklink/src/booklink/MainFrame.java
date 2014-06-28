854be8394003b4a5792813e93616281e791e3915
Eidträger <Eidtrger@Sula.congstar.box>
2014-06-28 23:48:12 +0200

    Jtable statt jlist

--- Booklink/src/booklink/MainFrame.java
+++ Booklink/src/booklink/MainFrame.java
@@ -5,9 +5,17 @@ package booklink;
 
 import booklink.gui.jpAddBook;
 import booklink.gui.jpAddPDF;
+import java.awt.Component;
 import java.awt.event.KeyEvent;
+import java.util.ArrayList;
+import java.util.List;
 import javax.swing.BoxLayout;
 import javax.swing.JPanel;
+import javax.swing.ListModel;
+import javax.swing.RowSorter;
+import javax.swing.SortOrder;
+import javax.swing.table.TableCellRenderer;
+import javax.swing.table.TableColumn;
 
 
 
@@ -18,10 +26,56 @@ public class MainFrame extends javax.swing.JFrame {
     public MainFrame() {
         initComponents();
         setExtendedState(java.awt.Frame.MAXIMIZED_BOTH);
+        displayTable();
+        
     }
 
     
-    
+   private void displayTable() {
+   jTable1.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {1, "Heinz", "Stephen King"},
+                {2, "Bernd", "Grass, G."},
+                {3, "Zeppelin", "Kohl, H."},
+                {4, "Schlumpf", "Picard, J-L."}
+            },
+            new String [] {
+                "Nr", "Titel", "Autor"
+            }
+        ));
+   
+    RowSorter sorter = jTable1.getRowSorter();
+    List sortKeys = new ArrayList();
+    sortKeys.add(new RowSorter.SortKey(1, SortOrder.ASCENDING));
+    sorter.setSortKeys(sortKeys);
+   
+   
+   
+   
+        jTable1.setAutoResizeMode(javax.swing.JTable.AUTO_RESIZE_OFF);
+        for (int column = 0; column < jTable1.getColumnCount(); column++) {
+            TableColumn tableColumn = jTable1.getColumnModel().getColumn(column);
+            int preferredWidth = tableColumn.getMinWidth();
+            int maxWidth = tableColumn.getMaxWidth();
+
+            for (int row = 0; row < jTable1.getRowCount(); row++) {
+                TableCellRenderer cellRenderer = jTable1.getCellRenderer(row, column);
+                Component c = jTable1.prepareRenderer(cellRenderer, row, column);
+                int width = c.getPreferredSize().width + jTable1.getIntercellSpacing().width;
+                preferredWidth = Math.max(preferredWidth, width);
+
+        //  We've exceeded the maximum width, no need to check other rows
+                if (preferredWidth >= maxWidth) {
+                    preferredWidth = maxWidth;
+                    break;
+                }
+            }
+
+            tableColumn.setPreferredWidth(preferredWidth);
+        }
+
+    }
+
    
     
     @SuppressWarnings("unchecked")
@@ -34,8 +88,8 @@ public class MainFrame extends javax.swing.JFrame {
         BuchButton1 = new javax.swing.JButton();
         multipanel = new javax.swing.JPanel();
         pdfpanel = new javax.swing.JPanel();
-        jScrollPane2 = new javax.swing.JScrollPane();
-        Booklist = new javax.swing.JList();
+        jScrollPane1 = new javax.swing.JScrollPane();
+        jTable1 = new javax.swing.JTable();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Booklink");
@@ -105,21 +159,32 @@ public class MainFrame extends javax.swing.JFrame {
         getContentPane().add(pdfpanel);
         pdfpanel.setBounds(270, 310, 710, 210);
 
-        Booklist.setBackground(new java.awt.Color(229, 255, 243));
-        Booklist.setModel(new javax.swing.AbstractListModel() {
-            String[] strings = { "Grundlagen der Informatik; Clark Kent, Lois Lane", "Programmieren 1; Thomas Müller, Philipp Lahm", "Programmieren 2", "Mathematik" };
-            public int getSize() { return strings.length; }
-            public Object getElementAt(int i) { return strings[i]; }
-        });
-        Booklist.addMouseListener(new java.awt.event.MouseAdapter() {
+        jTable1.setAutoCreateRowSorter(true);
+        jTable1.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Title 1", "Title 2", "Title 3", "Title 4"
+            }
+        ));
+        jTable1.setRowSelectionAllowed(true);
+        jTable1.setSelectionMode(javax.swing.ListSelectionModel.SINGLE_SELECTION);
+        jTable1.setShowHorizontalLines(false);
+        jTable1.setShowVerticalLines(false);
+        jTable1.getTableHeader().setResizingAllowed(false);
+        jTable1.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
-                BooklistMouseClicked(evt);
+                jTable1MouseClicked(evt);
             }
         });
-        jScrollPane2.setViewportView(Booklist);
+        jScrollPane1.setViewportView(jTable1);
 
-        getContentPane().add(jScrollPane2);
-        jScrollPane2.setBounds(30, 50, 230, 489);
+        getContentPane().add(jScrollPane1);
+        jScrollPane1.setBounds(0, 50, 240, 490);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
@@ -142,8 +207,7 @@ public class MainFrame extends javax.swing.JFrame {
 
     
     private void SearchFieldKeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_SearchFieldKeyPressed
-      //if(evt.getKeyCode()== KeyEvent.VK_ENTER)                                  // Suche + ENTER 
-         // jLabel1.setText("Gesucht wurde:  "+SearchField.getText());
+      
       
       
     }//GEN-LAST:event_SearchFieldKeyPressed
@@ -170,29 +234,13 @@ public class MainFrame extends javax.swing.JFrame {
                                                     // Fenster für PDfs anzeigen lassen      !!!!
     }//GEN-LAST:event_PDFMouseClicked
 
-    private void BooklistMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_BooklistMouseClicked
-        
-       jpAddBook bookpanel = new jpAddBook(true);
-       bookpanel.setAuthor(Booklist.getSelectedValue().toString());
-     //  Booklist.addListSelectionListener();
+    private void jTable1MouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jTable1MouseClicked
+       int row = jTable1.getSelectedRow();
+       int column = jTable1.getSelectedColumn();
        
-     //  bookpanel.setBooktitel(Booklist.getSelectedValue().toString());
-       bookpanel.setComment(Booklist.getSelectedValue().toString());
-       bookpanel.setEdition(Booklist.getSelectedValue().toString());
-       bookpanel.setISBN(Booklist.getSelectedValue().toString());
-       bookpanel.setLendingPeriod(Booklist.getSelectedValue().toString());
-       bookpanel.setPress(Booklist.getSelectedValue().toString());
-       bookpanel.setYear(Booklist.getSelectedValue().toString());
+        /// Funktion von Chris aufrufen ;) :D
        
-       multipanel.setVisible(true); 
-         multipanel.setLayout(new java.awt.BorderLayout()); 
-         multipanel.add(bookpanel); 
-         multipanel.validate();
-        // Aus Buchliste wählen
-        //jLabel2.setText("Gewählt wurde:  "+Booklist.getSelectedValue());        // Gewähltes Buch anzeigen lassen
-       
-       
-    }//GEN-LAST:event_BooklistMouseClicked
+    }//GEN-LAST:event_jTable1MouseClicked
 
     /**
      * @param args the command line arguments
@@ -231,12 +279,12 @@ public class MainFrame extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JList Booklist;
     private javax.swing.JButton BuchButton1;
     private javax.swing.JButton PDF;
     private javax.swing.JButton Search;
     private javax.swing.JTextField SearchField;
-    private javax.swing.JScrollPane jScrollPane2;
+    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JTable jTable1;
     private javax.swing.JPanel multipanel;
     private javax.swing.JPanel pdfpanel;
     // End of variables declaration//GEN-END:variables
